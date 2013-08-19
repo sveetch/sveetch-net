3b8a61f0b06737c363adc4609ed7c68fe471864d
David THENON <sveetch@gmail.com>
2013-08-19 05:04:21 +0200

    Suite intégration sur la partie /accounts/, update du buildout pour l'upgrade sur sveeaccounts

---
+++ project/mods_available/sveeaccounts/crispies.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+from crispy_forms.helper import FormHelper
+from crispy_forms_foundation.layout import Layout, Fieldset, Row, Column, ButtonHolder, Submit
+
+def LoginHelper():
+    """
+    Simple helper for the login form
+    """
+    helper = FormHelper()
+    helper.form_tag = False
+    helper.layout = Layout(
+        Row(
+            Column(
+                'username',
+                'password',
+                css_class='twelve'
+            ),
+        ),
+    )
+    
+    return helper
+
+def RegistrationFormHelper():
+    """
+    Helper for the user registration form
+    """
+    helper = FormHelper()
+    helper.form_action = '.'
+    helper.layout = Layout(
+        Row(
+            Column(
+                'email',
+                'username',
+                css_class='twelve'
+            ),
+        ),
+        Row(
+            Column(
+                'password1',
+                css_class='six'
+            ),
+            Column(
+                'password2',
+                css_class='six'
+            ),
+        ),
+        Row(
+            Column(
+                'captcha',
+                css_class='twelve'
+            ),
+        ),
+        Row(
+            Column(
+                ButtonHolder( Submit('submit', _('Submit')), css_class="text-right" ),
+                css_class='twelve'
+            ),
+        )
+    )
+    
+    return helper
--- project/mods_available/sveeaccounts/settings.py
+++ project/mods_available/sveeaccounts/settings.py
@@ -4,6 +4,11 @@ Settings to enable sveeaccounts
 """
 INSTALLED_APPS = add_to_tuple(INSTALLED_APPS, 'registration', 'captcha', 'sveeaccounts')
 
+# Custom helpers for the "accounts" forms
+REGISTRATION_FORM_HELPER = "project.mods_available.sveeaccounts.crispies.RegistrationFormHelper"
+
+REGISTRATION_LOGIN_HELPER = "project.mods_available.sveeaccounts.crispies.LoginHelper"
+
 """
 Settings for django-registration
 """
