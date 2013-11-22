c1d1cca9c5b73fd11c65711e19641f2df4cb36d5
David THENON <sveetch@gmail.com>
2013-11-23 00:43:11 +0100

    Adding production settings file, update gitignore

---
+++ project/settings_production.py
@@ -0,0 +1,32 @@
+"""
+Settings for production environment
+"""
+from project.settings import *
+
+DEBUG = TEMPLATE_DEBUG = False
+
+ASSETS_DEBUG = False
+ASSETS_ROOT = STATIC_ROOT
+ASSETS_AUTO_BUILD = False
+
+# Fast-cgi options
+FCGI_OPTIONS = {
+    'method': 'threaded',
+    'daemonize': 'false',
+}
+
+# SMTP Settings to send Applications emails
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_SUBJECT_PREFIX = '[${VARS:NAME}] '
+
+# Database access
+DATABASES = {
+    'default': {
+        'HOST': 'localhost',
+        'NAME': '${VARS:NAME}',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'sveetch',
+        'PASSWORD': '',
+    }
+}
