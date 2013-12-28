cf84c727c07fcf64031149b2a2763dd49377b833
David THENON <sveetch@gmail.com>
2013-12-28 20:17:08 +0100

    Some changes on app.js according to the update of crispy foundation to 0.2.4.1 to restore behavior on filter checkboxes in url archives

--- project/webapp_statics/js/foundation/app.js
+++ project/webapp_statics/js/foundation/app.js
@@ -14,10 +14,8 @@ $(document).ready(function() {
     // Automatically add "no-reset-click" class on direct input parent label to 
     // follow their natural behavior (to propagate the click to their input child, 
     // usually only for radio or checkbox)
-    $("form .button.dropdown .no-reset-click").each(function(index) {
-        if($(this).parent().prop('nodeName')=='LABEL'){
-            $(this).parent().addClass('no-reset-click');
-        }
+    $("form .button.dropdown label.no-reset-click input").each(function(index) {
+        $(this).addClass('no-reset-click');
     });
     
     //$.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
